5b6acd9a5a9b39a81d3f4e70e3237832ebff0975
Mandalore477 <sithlordjazz@gmail.com>
2019-12-18 15:36:01 -0500

    implemented continuous combat and surrender

--- Assets/Scripts/CombatManager.cs
+++ Assets/Scripts/CombatManager.cs
@@ -54,6 +54,14 @@ public class CombatManager : MonoBehaviour
     Text atk_health;
     [SerializeField]
     Text def_health;
+    [SerializeField]
+    Button p1_atk_but;
+    [SerializeField]
+    Button p1_sur_but;
+    [SerializeField]
+    Button p2_atk_but;
+    [SerializeField]
+    Button p2_sur_but;
 
     public float phase_timer_fl = 3.0f;
     float combat_timer =0;
@@ -64,6 +72,11 @@ public class CombatManager : MonoBehaviour
     public bool is_defender_win;
     public bool is_p1_turn;
     public bool is_card_selected;
+    bool is_p1_atk;
+    bool is_p1_sur;
+    bool is_p2_atk;
+    bool is_p2_sur;
+    bool is_p1_attacker;
 
 
     // Start is called before the first frame update
@@ -82,9 +95,15 @@ public class CombatManager : MonoBehaviour
         gman=FindObjectOfType<BoardGameManager>();
         gman.cman = transform.parent.gameObject;
         if (gman.playerturn == 1)
+        {
             is_p1_turn = true;
+            is_p1_attacker = true;
+        }
         else
+        {
             is_p1_turn = false;
+            is_p1_attacker = false;
+        }
         is_declare_horde = true;
         is_card_selected = false;
         fight_canvas.gameObject.SetActive(false);
@@ -108,7 +127,36 @@ public class CombatManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (is_declare_horde)
+        if (!is_p1_atk)
+        {
+            p1_hand_canvas.gameObject.SetActive(true);
+            p2_hand_canvas.gameObject.SetActive(false);
+            p1_atk_but.gameObject.SetActive(true);
+            p1_sur_but.gameObject.SetActive(true);
+        }
+        else if (!is_p2_atk)
+        {
+            p2_hand_canvas.gameObject.SetActive(true);
+            p1_hand_canvas.gameObject.SetActive(false);
+            p2_atk_but.gameObject.SetActive(true);
+            p2_sur_but.gameObject.SetActive(true);
+        }
+        if (is_p1_attacker && is_p1_sur)
+        {
+            gman.LoseBattle();
+        }else if(is_p1_attacker && is_p2_sur)
+        {
+            gman.WinBattle();
+        }
+        else if (!is_p1_attacker && is_p1_sur)
+        {
+            gman.WinBattle();
+        }
+        else if (!is_p1_attacker && is_p2_sur)
+        {
+            gman.LoseBattle();
+        }
+        if (is_declare_horde && is_p1_atk && is_p2_atk)
         {
         DisplayP1Hand();
         DisplayP2Hand();
@@ -126,6 +174,10 @@ public class CombatManager : MonoBehaviour
                     else if (p1_stack_left.Count > 0)
                     {
                         p1_stack_mid = p1_stack_left.Pop();
+                    }
+                    else if (p1_stack_left.Count == 0 && p1_stack_right.Count == 0)
+                    {
+                        p1_stack_mid = null;
                     }
                     is_p1_turn = false;
                     attacker.cur_health = attacker.health + P1BuffHealth;
@@ -146,6 +198,10 @@ public class CombatManager : MonoBehaviour
                     else if (p2_stack_left.Count > 0)
                     {
                         p2_stack_mid = p2_stack_left.Pop();
+                    }
+                    else if (p2_stack_left.Count == 0 && p2_stack_right.Count == 0)
+                    {
+                        p2_stack_mid = null;
                     }
                     is_p1_turn = true;
                     attacker.cur_health = attacker.health + P2BuffHealth;
@@ -168,6 +224,10 @@ public class CombatManager : MonoBehaviour
                     {
                         p1_stack_mid = p1_stack_left.Pop();
                     }
+                    else if (p1_stack_left.Count == 0 && p1_stack_right.Count == 0)
+                    {
+                        p1_stack_mid = null;
+                    }
                     is_p1_turn = false;
                     defender.cur_health = defender.health + P1BuffHealth;
                     defender.add_dmg = defender.add_dmg + P1BuffDamage;
@@ -185,6 +245,9 @@ public class CombatManager : MonoBehaviour
                     else if (p2_stack_left.Count > 0)
                     {
                         p2_stack_mid = p2_stack_left.Pop();
+                    }else if(p2_stack_left.Count==0 && p2_stack_right.Count == 0)
+                    {
+                        p2_stack_mid = null;
                     }
                     is_p1_turn = true;
                     defender.cur_health = defender.health + P2BuffHealth;
@@ -237,6 +300,10 @@ public class CombatManager : MonoBehaviour
                         gman.MonsterDiscard.Add(attacker);
                         attacker = null;
                     }
+                    if (is_p1_attacker)
+                    {
+                        is_p1_turn = true;
+                    }else { is_p1_turn = false; }
                 }
                 else
                 {
@@ -253,6 +320,11 @@ public class CombatManager : MonoBehaviour
                         gman.MonsterDiscard.Add(defender);
                         defender = null;
                     }
+                    if (is_p1_attacker)
+                    {
+                        is_p1_turn = true;
+                    }
+                    else { is_p1_turn = false; }
                 }
             }
         }
@@ -267,19 +339,84 @@ public class CombatManager : MonoBehaviour
                 if (is_defender_win)
                 {
                     //defender win
-                    is_declare_horde = true;
-                    is_combat_completed = false;
-                    is_p1_turn = true;
-                    gman.LoseBattle();
+                    is_p1_atk = false;
+                    is_p2_atk = false;
+                    if (!is_p1_atk && is_p1_attacker)
+                    {
+                        p1_hand_canvas.gameObject.SetActive(true);
+                        p2_hand_canvas.gameObject.SetActive(false);
+                        p1_atk_but.gameObject.SetActive(true);
+                        p1_sur_but.gameObject.SetActive(true);
+                        is_declare_horde = true;
+                        is_combat_completed = false;
+                    }
+                    else if (!is_p2_atk && !is_p1_attacker)
+                    {
+                        p2_hand_canvas.gameObject.SetActive(true);
+                        p1_hand_canvas.gameObject.SetActive(false);
+                        p2_atk_but.gameObject.SetActive(true);
+                        p2_sur_but.gameObject.SetActive(true);
+                        is_declare_horde = true;
+                        is_combat_completed = false;
+                    }
+                    if (is_p1_attacker && (is_p1_sur || p1_cards.Count==0))
+                    {
+                        gman.LoseBattle();
+                    }
+                    else if (is_p1_attacker && is_p2_sur)
+                    {
+                        gman.WinBattle();
+                    }
+                    else if (!is_p1_attacker && is_p1_sur)
+                    {
+                        gman.WinBattle();
+                    }
+                    else if (!is_p1_attacker && is_p2_sur)
+                    {
+                        gman.LoseBattle();
+                    }
 
                 }
                 else
                 {
                     //attacker win
-                    is_declare_horde = true;
-                    is_combat_completed = false;
-                    is_p1_turn = true;
-                    gman.WinBattle();
+                    is_p1_atk = false;
+                    is_p2_atk = false;
+                    if (!is_p1_atk && is_p1_attacker)
+                    {
+                        p1_hand_canvas.gameObject.SetActive(true);
+                        p2_hand_canvas.gameObject.SetActive(false);
+                        p1_atk_but.gameObject.SetActive(true);
+                        p1_sur_but.gameObject.SetActive(true);
+                        is_declare_horde = true;
+                        is_combat_completed = false;
+                    }
+                    else if (!is_p2_atk && !is_p1_attacker)
+                    {
+                        p2_hand_canvas.gameObject.SetActive(true);
+                        p1_hand_canvas.gameObject.SetActive(false);
+                        p2_atk_but.gameObject.SetActive(true);
+                        p2_sur_but.gameObject.SetActive(true);
+                        is_declare_horde = true;
+                        is_combat_completed = false;
+                    }
+                    if (is_p1_attacker && (is_p1_sur || !p1_stack_mid))
+                    {
+                        gman.LoseBattle();
+                    }
+                    else if (is_p1_attacker && (is_p2_sur || !p2_stack_mid))
+                    {
+                        gman.WinBattle();
+                    }
+                    else if (!is_p1_attacker && (is_p1_sur || !p1_stack_mid))
+                    {
+                        gman.WinBattle();
+                    }
+                    else if (!is_p1_attacker && (is_p2_sur || !p2_stack_mid))
+                    {
+                        gman.LoseBattle();
+                    }
+                    
                 }
             }
         }
@@ -352,8 +489,33 @@ public class CombatManager : MonoBehaviour
         is_card_selected = true;
     }
 
+    void ReturnHandsToPLayer()
+    {
+        gman.player1.cardsHand.RemoveRange(0, gman.player1.cardsHand.Count);
+        for(int i = 0; i < p1_stack_left.Count; i++)
+        {
+            gman.player1.cardsHand.Add(p1_stack_left.Pop());
+        }
+        gman.player1.cardsHand.Add(p1_stack_mid);
+        for (int i = 0; i < p1_stack_right.Count; i++)
+        {
+            gman.player1.cardsHand.Add(p1_stack_right.Pop());
+        }
+        gman.player2.cardsHand.RemoveRange(0, gman.player2.cardsHand.Count);
+        for (int i = 0; i < p2_stack_left.Count; i++)
+        {
+            gman.player2.cardsHand.Add(p2_stack_left.Pop());
+        }
+        gman.player2.cardsHand.Add(p2_stack_mid);
+        for (int i = 0; i < p2_stack_right.Count; i++)
+        {
+            gman.player2.cardsHand.Add(p2_stack_right.Pop());
+        }
+    }
     void DisplayP1Hand()
     {
+        p1_atk_but.gameObject.SetActive(false);
+        p1_sur_but.gameObject.SetActive(false);
         if (is_p1_turn) 
         { 
             p1_hand_canvas.gameObject.SetActive(true);
@@ -394,6 +556,8 @@ public class CombatManager : MonoBehaviour
 
     void DisplayP2Hand()
     {
+        p2_atk_but.gameObject.SetActive(false);
+        p2_sur_but.gameObject.SetActive(false);
         if (!is_p1_turn)
         {
             p2_hand_canvas.gameObject.SetActive(true);
@@ -432,4 +596,21 @@ public class CombatManager : MonoBehaviour
         }
     }
 
+    public void P1Attack()
+    {
+        is_p1_atk = true;
+    }
+
+    public void P1Surrender()
+    {
+        is_p1_sur = true;
+    }
+    public void P2Attack()
+    {
+        is_p2_atk = true;
+    }
+    public void P2Surrender()
+    {
+        is_p2_sur = true;
+    }
 }
